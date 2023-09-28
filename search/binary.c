012344bb0f56c8bc40b634c70841312fa0f75bba
Kishor U <upadhyaya13kishor@gmail.com>
2023-09-28 21:07:24 +0545

    reformatted binary

--- search/binary.c
+++ search/binary.c
@@ -1,29 +1,32 @@
 // binary search
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int binary_search(int *a, int n, int key)
+// binary search algorithm
+int binary_search(int *arr, int size, int key)
 {
     int low, high, mid;
     low = 0;
-    high = n - 1;
-    while (low <= high) {
-        mid = (low + high)/2;
-        if (key < a[mid])
-            high = mid - 1;
-        else if (key > a[mid])
-            low = mid + 1;
-        else
-            return mid;
+    high = size - 1;
+    while (low <= high)
+    {
+        mid = (low + high) / 2;  // calculate the middle index of the array
+        if (key < arr[mid])      // if the key is less than the middle element
+            high = mid - 1;      // update the high index to search the left half of the array
+        else if (key > arr[mid]) // if the key is greater than the middle element
+            low = mid + 1;       // update the low index to search the right half of the array
+        else                     // if the key is equal to the middle element
+            return mid;          // return the index of the middle element
     }
-    return -1;
+    return -1; // return -1 if the key is not found in the array
 }
 
 int main()
 {
-    int a[10] = {1, 2, 3, 4, 5, 6, 7};
-    int key = 5;
-    int index = binary_search(a, 7, key);
-    printf("index = %d\n", index);
-    return 0;
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7};    // initialize an array of integers
+    int key = 5;                            // initialize the key to be searched for
+    int index = binary_search(arr, 7, key); // call the binary_search function to search for the key in the array
+    printf("index = %d\n", index);          // print the index of the key in the array
+    return 0;                               // return 0 to indicate successful execution
 }
